a02e6c7f5db84c7928861fa038f65d803d399526
as4348 <141130059+as4348@users.noreply.github.com>
2026-01-12 12:27:17 +0530

    Update test_dependencies.py

--- CV folder/test_dependencies.py
+++ CV folder/test_dependencies.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
-print("✅ OpenCV and NumPy are working correctly in VS Code!")
+print("OpenCV and NumPy are working correctly in VS Code!")
 print("OpenCV version:", cv2.__version__)
 print("NumPy version:", np.__version__)
+
